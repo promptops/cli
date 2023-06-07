7a120b5e0ceb68c1b1416a109182da53b2c4a673
JJ <jj@ctrlstack.com>
2023-06-06 19:33:55 -0700

    fix bug with removing indexed url-s

--- promptops/index/entry_point.py
+++ promptops/index/entry_point.py
@@ -2,16 +2,22 @@ import os.path
 
 from promptops.similarity import embedding
 from promptops import settings
+from promptops.feedback import feedback
 
 from .index_store import IndexStore
 from .content import index_url, index_file
+
+
+def is_url(source):
+    return source.startswith("http://") or source.startswith("https://")
 
 
 def entry_point(args):
     if args.action == "add":
         source_path = args.source
         print("indexing:", source_path)
-        if args.source.startswith("http://") or args.source.startswith("https://"):
+        feedback({"event": "index_add", "path": source_path})
+        if is_url(source_path):
             item_meta, db = index_url(source_path)
         else:
             item_meta, db = index_file(source_path)
@@ -30,10 +36,16 @@ def entry_point(args):
         for item in store.metadata:
             print(f" {'  Y' if item.watch else ' ':5} │ {item.item_type:4} │ {item.item_location:80} │ {item.last_indexed_on:%Y-%m-%d %H:%M:%S}")
     elif args.action == "remove":
-        source_path = os.path.abspath(args.source)
+        source_path = args.source
+        feedback({"event": "index_remove", "path": source_path})
+        if is_url(source_path):
+            item_type = "url"
+        else:
+            item_type = "file"
+            source_path = os.path.abspath(source_path)
         store = IndexStore(os.path.expanduser(settings.user_index_root))
         for ix, item in enumerate(store.metadata):
-            if item.item_location == source_path:
+            if item.item_location == source_path and item.item_type == item_type:
                 break
         else:
             print(f"item not found: {source_path}")
--- promptops/nux.py
+++
@@ -1,2 +0,0 @@
-def entry_point():
-    pass
