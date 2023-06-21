45ff5228aaac667a81a5cf024c7cafe7ac132c33
Josh White <josh@ctrlstack.com>
2023-06-21 14:47:09 -0500

    integrate suggest_next_near

--- promptops/skills/next.py
+++ promptops/skills/next.py
@@ -1,9 +1,9 @@
-from promptops.query.suggest_next import suggest_next_suffix, suggest_next_gpt, deduplicate, pretty_result
+from promptops.query.suggest_next import suggest_next_suffix, suggest_next_suffix_near, suggest_next_gpt, deduplicate, pretty_result
 from .choice import Choice
 
 
 def instant_choices(n: int = 4) -> list[Choice]:
-    options = deduplicate(suggest_next_suffix(n))
+    options = deduplicate(suggest_next_suffix(n // 2) + suggest_next_suffix_near(n // 2))
     return [Choice("command", pretty_result(option), option) for option in options]
 
 
