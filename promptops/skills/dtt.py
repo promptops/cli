2cf30a079bc5df97f245186dc0f513954d2ba6b7
Josh White <mrjoshualwhite@gmail.com>
2023-06-23 10:39:14 -0500

    if I ctrl-c in dtt mode be done

--- promptops/skills/dtt.py
+++ promptops/skills/dtt.py
@@ -62,7 +62,7 @@ def entry_point():
             handle(options[selected])
             print()
         except KeyboardInterrupt:
-            pass
+            return
 
 
 def done_callback(lock, ui: selections.UI, options: list[Choice], counter: Counter):
