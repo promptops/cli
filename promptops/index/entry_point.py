8f0cec9b65f6245cb5e9830b9096b5dc86751900
JJ <jj@ctrlstack.com>
2023-06-30 19:52:08 -0700

    update index list with terminal size awareness and ellipsis for long locations

--- promptops/index/entry_point.py
+++ promptops/index/entry_point.py
@@ -1,4 +1,5 @@
 import os.path
+import shutil
 
 from promptops.similarity import embedding
 from promptops import settings
@@ -10,6 +11,13 @@ from .content import index_url, index_file
 
 def is_url(source):
     return source.startswith("http://") or source.startswith("https://")
+
+
+def mid_ellipsis(s: str, max_len: int) -> str:
+    if len(s) <= max_len:
+        return s
+    mid_len = (max_len - 3) // 2
+    return s[:mid_len] + "..." + s[-(max_len - mid_len - 3):]
 
 
 def entry_point(args):
@@ -30,11 +38,15 @@ def entry_point(args):
         for item in items:
             print(item)
     elif args.action == "list":
+        size = shutil.get_terminal_size()
+        watch_width = len(" Watch ")
+        last_indexed_width = max(len(" Last Indexed "), len(" 2020-01-01 00:00:00 "))
+        location_width = size.columns - watch_width - last_indexed_width - 2
         store = IndexStore(os.path.expanduser(settings.user_index_root))
-        print(f" Watch │ Type │ {'Location':80} │ Last Indexed")
-        print(f" {'─' * 6}┼{'─' * 6}┼{'─' * 82}┼{'─' * 19}")
+        print(f" Watch │{' Location'.ljust(location_width)}│ Last Indexed ")
+        print(f"{'─' * watch_width}┼{'─' * location_width}┼{'─' * last_indexed_width}")
         for item in store.metadata:
-            print(f" {'  Y' if item.watch else ' ':5} │ {item.item_type:4} │ {item.item_location:80} │ {item.last_indexed_on:%Y-%m-%d %H:%M:%S}")
+            print(f"{('Y' if item.watch else '').center(watch_width)}│ {mid_ellipsis(item.item_location, location_width-1).ljust(location_width-1)}│ {item.last_indexed_on:%Y-%m-%d %H:%M:%S} ")
     elif args.action == "remove":
         source_path = args.source
         feedback({"event": "index_remove", "path": source_path})
